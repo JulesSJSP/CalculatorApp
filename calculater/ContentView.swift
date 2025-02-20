b0b075d0bfe4071c5e493287799fdcbb0dd13044
julesSJSP <jules.beausaert@jopie.be>
2025-02-20 12:08:42 +0100

    afgemaakt

--- calculater/ContentView.swift
+++ calculater/ContentView.swift
@@ -1,51 +1,220 @@
-//
-//  ContentView.swift
-//  calculater
-//
-//  Created by chapter 2 on 20/02/2025.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Color.black.ignoresSafeArea()
-        HStack {
-            Button(action: {
+        ZStack {
+            Color.black.ignoresSafeArea()
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Text("0 ")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .frame(width: 150, height: 100)
+                        .foregroundColor(.white)
+                }
+                HStack(spacing: 20) {
+                    Button(action: {
+                       
+                    }) {
+                        Text("AC")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 80, height: 80)
+                            .background(Circle().fill(Color.gray))
+                            .cornerRadius(50)
+                    }
+               
+                    Text("+/-")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 80, height: 80)
+                        .background(Circle().fill(Color.gray))
+                        .cornerRadius(50)
+                    
+                    Text("%")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 80, height: 80)
+                        .background(Circle().fill(Color.gray))
+                        .cornerRadius(50)
+                    
+                    Text("/")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 80, height: 80) // Grotere knop
+                        .background(Circle().fill(Color.orange))
+                        .cornerRadius(50)
+                }
+                .background(.black)
                 
-            }) {
-                Text("rekenmachine")
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(.gray)
-                    .cornerRadius(50)
-                Spacer()
-                Text("0")
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(.gray)
-                    .cornerRadius(50)
-                Spacer()
-                Text(",")
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(.gray)
-                    .cornerRadius(50)
-                Spacer()
-                Text("=")
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(.orange)
-                    .cornerRadius(50)
-                Spacer()
+                HStack(spacing: 20) {
+                    Button(action: {
+                       
+                    }) {
+                        Text("7 ")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 80, height: 80) // Grotere knop
+                            .background(Circle().fill(Color.gray))
+                            .cornerRadius(50)
+                    }
+                
+                    Text("8")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 80, height: 80) // Grotere knop
+                        .background(Circle().fill(Color.gray))
+                        .cornerRadius(50)
+                    
+                    Text("9")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 80, height: 80) // Grotere knop
+                        .background(Circle().fill(Color.gray))
+                        .cornerRadius(50)
+                    
+                    Text("x")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 80, height: 80) // Grotere knop
+                        .background(Circle().fill(Color.orange))
+                        .cornerRadius(50)
+                    
+                }
+                .background(.black)
+                HStack(spacing: 20) {
+                    Button(action: {
+                       
+                    }) {
+                        Text("4")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 80, height: 80) // Grotere knop
+                            .background(Circle().fill(Color.gray))
+                            .cornerRadius(50)
+                    }
+                    
+                    Text("5")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 80, height: 80) // Grotere knop
+                        .background(Circle().fill(Color.gray))
+                        .cornerRadius(50)
+                    
+                    Text("6")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 80, height: 80) // Grotere knop
+                        .background(Circle().fill(Color.gray))
+                        .cornerRadius(50)
+                    
+                    Text("-")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 80, height: 80) // Grotere knop
+                        .background(Circle().fill(Color.orange))
+                        .cornerRadius(50)
+                    
+                }
+                .background(.black)
+                
+                HStack(spacing: 20) {
+                    Button(action: {
+                        
+                    }) {
+                        Text("1")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 80, height: 80) // Grotere knop
+                            .background(Circle().fill(Color.gray))
+                            .cornerRadius(50)
+                    }
+                    
+                    Text("2")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 80, height: 80) // Grotere knop
+                        .background(Circle().fill(Color.gray))
+                        .cornerRadius(50)
+                    
+                    Text("3")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 80, height: 80) // Grotere knop
+                        .background(Circle().fill(Color.gray))
+                        .cornerRadius(50)
+                    
+                    Text("+")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 80, height: 80)
+                        .background(Circle().fill(Color.orange))
+                        .cornerRadius(50)
+                    
+                }
+                
+                HStack(spacing: 20) {
+                    Button(action: {
+                        
+                    }) {
+                        Text("r")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 80, height: 80) // Grotere knop
+                            .background(Circle().fill(Color.gray))
+                            .cornerRadius(50)
+                    }
+                    
+                    Text("0")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 80, height: 80) // Grotere knop
+                        .background(Circle().fill(Color.gray))
+                        .cornerRadius(50)
+                    
+                    Text(",")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 80, height: 80) // Grotere knop
+                        .background(Circle().fill(Color.gray))
+                        .cornerRadius(50)
+                    
+                    Text("=")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 80, height: 80) // Grotere knop
+                        .background(Circle().fill(Color.orange))
+                        .cornerRadius(50)
+                    
+                }
             }
-        }.background(.black)
-
+            .background(.black)
+        }
     }
 }
-
-
-  
 
 #Preview {
     ContentView()
