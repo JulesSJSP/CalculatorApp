7441f5619f0c822d098b4902cb01908a5e18b8fd
julesSJSP <jules.beausaert@jopie.be>
2025-02-20 10:49:10 +0100

    Initial Commit

---
+++ calculater/calculaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculaterApp.swift
+//  calculater
+//
+//  Created by chapter 2 on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct calculaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
